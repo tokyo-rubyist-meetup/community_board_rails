fc707749e89a8e1f1c72812a37f569d92676f33f
Paul McMahon <paul@mobalean.com>
2013-03-02 19:19:45 +0900

    need communities for json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CommunityBoard::Application.routes.draw do
   devise_for :users
-  resources :communities, except: :index do
+  resources :communities do
     resources :posts
   end
   root to: 'communities#index'
