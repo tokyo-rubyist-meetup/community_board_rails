717279303e70832d06e4f83a894fd89a75d120eb
Paul McMahon <paul@mobalean.com>
2013-03-10 14:01:55 +0900

    clean up spec

--- spec/acceptance/api_spec.rb
+++ spec/acceptance/api_spec.rb
@@ -1,61 +1,44 @@
 require 'acceptance/acceptance_helper'
 require 'capybara/json'
 
-feature 'OAuth authorization' do
+feature 'API' do
+  include Capybara::Json
+  background { Capybara.current_driver = :httpclient_json }
   let(:app) { create(:application) }
   let(:user) { create(:user) }
-
-  scenario 'auth ok' do
-    client = OAuth2::Client.new(app.uid, app.secret) do |b|
+  let(:client) do
+    OAuth2::Client.new(app.uid, app.secret) do |b|
       b.request :url_encoded
       b.adapter :rack, Rails.application
     end
-    token = client.password.get_token(user.email, user.password)
-    token.should_not be_expired
   end
+  let(:token) { client.password.get_token(user.email, user.password) }
 
-  scenario 'auth nok' do
-    client = OAuth2::Client.new(app.uid, app.secret) do |b|
-      b.request :url_encoded
-      b.adapter :rack, Rails.application
+  scenario('auth ok') { token.should_not be_expired }
+
+  scenario('auth nok') { -> {client.password.get_token(user.email, "123")}.should raise_error(OAuth2::Error) }
+
+  context "communities" do
+    let!(:community) { create(:community) }
+    scenario 'index' do
+      get '/api/v1/communities'
+      json["communities"].first["id"].should == community.id
     end
-    lambda {client.password.get_token(user.email, "123")}.should raise_error(OAuth2::Error)
-  end
-end
-
-feature 'communities' do
-  include Capybara::Json
-  before { Capybara.current_driver = :httpclient_json }
-  let!(:community) { create(:community) }
-  scenario 'index' do
-    get '/api/v1/communities'
-    json["communities"].first["id"].should == community.id
-  end
-end
-
-feature 'posts' do
-  include Capybara::Json
-  before { Capybara.current_driver = :httpclient_json }
-  let(:community) { community_post.community }
-  let!(:community_post) { create(:post) }
-  scenario 'index' do
-    get "/api/v1/communities/#{community.id}/posts"
-    json["posts"].first["id"].should == community_post.id
   end
 
-  let(:app) { create(:application) }
-  let(:user) { create(:user) }
+  context "posts" do
+    let(:community) { community_post.community }
+    let!(:community_post) { create(:post) }
 
-  scenario 'auth ok' do
-    client = OAuth2::Client.new(app.uid, app.secret) do |b|
-      b.request :url_encoded
-      b.adapter :rack, Rails.application
+    scenario 'index' do
+      get "/api/v1/communities/#{community.id}/posts"
+      json["posts"].first["id"].should == community_post.id
     end
-    token = client.password.get_token(user.email, user.password)
-    token.should_not be_expired
-    post "/api/v1/communities/#{community.id}/posts", post: { text: "foo"}, access_token: token.token
-    #token.post "/api/v1/communities/#{community.id}/posts", body: { text: "foo"}
-    user.posts.count.should == 1
-    user.posts.first.text.should == "foo"
+
+    scenario 'create' do
+      post "/api/v1/communities/#{community.id}/posts", post: { text: "foo"}, access_token: token.token
+      user.posts.count.should == 1
+      user.posts.first.text.should == "foo"
+    end
   end
 end
